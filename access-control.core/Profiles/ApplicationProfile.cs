9504886b90e690c4561d9c63af8cb11aacd95655
DeraE2024 <careers.joseph@yahoo.com>
2024-08-04 02:14:57 +0100

    add automapping profile

---
+++ access-control.core/Profiles/ApplicationProfile.cs
@@ -0,0 +1,11 @@
+﻿using AutoMapper;
+
+namespace access_control.core.Profiles
+{
+    public class ApplicationProfile : Profile
+    {
+        public ApplicationProfile()
+        {
+        }
+    }
+}
