661030d20c9e6ed9783296fe66e287882a1e4520
DeraE2024 <careers.joseph@yahoo.com>
2024-08-04 19:16:45 +0100

    add migration - change column IsLocked to IsOpen

--- access-control.domain/Entities/Lock.cs
+++ access-control.domain/Entities/Lock.cs
@@ -3,7 +3,7 @@
     public class Lock : BaseEntity
     {
         public string Name { get; set; }
-        public bool IsLocked { get; set; }
+        public bool IsOpen { get; set; }
         public string? TenantId { get; set; }
         public string SerialNumber { get; set; }
     }
