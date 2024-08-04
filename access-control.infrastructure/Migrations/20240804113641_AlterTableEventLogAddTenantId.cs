256f21107b95c48ca9dbc44c99a7cd1e1ba971c5
DeraE2024 <careers.joseph@yahoo.com>
2024-08-04 13:06:39 +0100

    add migration - update table eventlogs

---
+++ access-control.infrastructure/Migrations/20240804113641_AlterTableEventLogAddTenantId.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace access_control.infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AlterTableEventLogAddTenantId : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "TenantId",
+                table: "EventLogs",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "TenantId",
+                table: "EventLogs");
+        }
+    }
+}
