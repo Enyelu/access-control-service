53eb86320fac1f0554e17d7875118878b759e932
DeraE2024 <careers.joseph@yahoo.com>
2024-08-05 02:07:48 +0100

    add migration - add complaints table

---
+++ access-control.domain/Entities/Complaint.cs
@@ -0,0 +1,9 @@
+﻿namespace access_control.domain.Entities
+{
+    public class Complaint : BaseEntity
+    {
+        public string Email { get; set; }
+        public string Subject { get; set; }
+        public string Message { get; set; }
+    }
+}
---
+++ access-control.infrastructure/Migrations/20240805010502_AddTableComplaints.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace access_control.infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddTableComplaints : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Complaints",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Subject = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Message = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CreatedBy = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    ModifiedBy = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    ModifiedAt = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Complaints", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Complaints");
+        }
+    }
+}
